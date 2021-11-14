8694f177ed1311575df4f140a72caa8548ada4d3
Mohamad-zarzar <zarzarmoh993@gmail.com>
2021-11-14 15:48:46 +0100

    Projektrahmen erzeugt

---
+++ SP_Example/src/main/java/com/example/demo/SpExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpExampleApplication.class, args);
+	}
+
+}
