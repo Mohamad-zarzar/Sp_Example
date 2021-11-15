d10a6f675d9bf9da792a4fdae155724e7fd8bdf0
Mohamad-zarzar <zarzarmoh993@gmail.com>
2021-11-15 20:23:52 +0100

    JUnit Test

--- SP_Example/src/main/java/com/example/demo/SpExampleApplication.java
+++ SP_Example/src/main/java/com/example/demo/SpExampleApplication.java
@@ -2,11 +2,14 @@ package com.example.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.availability.ApplicationAvailability;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
@@ -14,20 +17,24 @@ public class SpExampleApplication implements ApplicationRunner{
 	
 	private static final Logger LOG = LoggerFactory.getLogger(SpExampleApplication.class);
 	
-	@Autowired
+	
 	private ApplicationContext applicationContext;
 	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpExampleApplication.class, args);
 	}
+	@Autowired
+	public SpExampleApplication(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		for(String beanName : applicationContext.getBeanDefinitionNames()){
-			System.out.println(beanName);
+		ApplicationAvailability availability = applicationContext.getBean(ApplicationAvailability.class);
+		LOG.info("Ready: {}, Live: {}", availability.getReadinessState(), availability.getLivenessState());
 		}
 		
 	}
 
-}
+
--- SP_Example/src/test/java/com/example/demo/SpExampleApplicationTests.java
+++ SP_Example/src/test/java/com/example/demo/SpExampleApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Assertions;
+//import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpExampleApplicationTests {
 
+	@Autowired
+	private SpExampleApplication app;
 	@Test
-	void contextLoads() {
+	void shoudBeNotNull() {
+		Assertions.assertNotNull(app);
+		
 	}
 
 }
