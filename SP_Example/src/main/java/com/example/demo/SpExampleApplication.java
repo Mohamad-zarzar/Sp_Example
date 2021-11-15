5f72d6b7be9e80cb94e81b92a4e6be345842b4cf
Mohamad-zarzar <zarzarmoh993@gmail.com>
2021-11-15 17:39:31 +0100

    Test hinzufügen

--- SP_Example/src/main/java/com/example/demo/SpExampleApplication.java
+++ SP_Example/src/main/java/com/example/demo/SpExampleApplication.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class SpExampleApplication {
+public class SpExampleApplication implements ApplicationRunner{
+	
+	@Autowired
+	private ApplicationContext applicationContext;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpExampleApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		for(String beanName : applicationContext.getBeanDefinitionNames()){
+			System.out.println(beanName);
+		}
+		
 	}
 
 }
